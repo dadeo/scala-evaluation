bd982a7723464c832b64e7a76210da06684167c8
dadeo <drobertwatson@gmail.com>
2011-01-14 11:35:08 -0600

    initial webapp with view that isn't found by jetty

---
+++ web/src/main/scala/d4d30/scala/evaluation/HelloController.scala
@@ -0,0 +1,13 @@
+package d4d30.scala.evaluation
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+class HelloController {
+
+  @RequestMapping(Array("/hello"))
+  def greet = "index"
+  
+}
+
